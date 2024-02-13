2ed2fa6c62507607040aa437c2b32e0cc5f13b8c
Štěpán Velínský <stteranv@gmail.com>
2024-02-13 01:34:02 +0100

    Add filter function + relocate imgs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
+import { useState } from "react"
+import allMovies from "./data"
+import categories from "./categories"
 
 const App = () => {
+  const {typeOfMovie, setTypeOfMovie} = useState("komedie")
+
+  const resultFilms = allMovies.filter( (oneMovie) => {
+      return oneMovie["category"] === typeOfMovie
+  } )
 
   return<>
-    <h1>Test</h1>
+    
   </>
 }
 
